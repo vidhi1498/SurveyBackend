7d47c9874c4dde6e15a30c0a2e27740bff54aa42
vidhi jain <31841095+vidhi1498@users.noreply.github.com>
2020-07-10 00:48:12 +0530

    First commit

---
+++ src/main/java/com/jwtauthentication/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.jwtauthentication.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jwtauthentication.model.Role;
+import com.jwtauthentication.model.RoleName;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
